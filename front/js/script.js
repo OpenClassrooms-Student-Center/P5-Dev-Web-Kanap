6c921dae42dd04dd91bbb83a7554294a654323d1
Chedli HAMMAMI <c.hammami@live.fr>
2022-02-18 12:33:51 +0100

    affichage des produits de l'API

---
+++ front/js/script.js
@@ -0,0 +1,31 @@
+const section = document.getElementById("items");
+const panierLocalStorage = localStorage.getItem("panier");
+const deletePanier = document.querySelector(".deletePanier");
+
+const response = async function () {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      for (const kanap of data) {
+        let article = `<a href="./product.html?id=${kanap._id}">
+        <article>
+          <img
+            src="${kanap.imageUrl}"
+            alt="${kanap.altTxt}"
+          />
+          <h3 class="productName">${kanap.name}</h3>
+          <p class="productDescription">
+            ${kanap.description}
+          </p>
+        </article>
+      </a>`;
+        section.innerHTML += article;
+      }
+    })
+    .catch((e) => {
+      console.log(data.status);
+    });
+};
+response();
