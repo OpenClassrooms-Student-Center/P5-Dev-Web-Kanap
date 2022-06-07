b641b5bdb04b203f8f4589d2b2b4609090754a4a
ClementFra <franquart.clemnt@gmail.com>
2022-06-08 00:27:28 +0200

    rending confirmation addtocart

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,11 @@
+// single product 
 var str = window.location.href;
 var url = new URL(str);
 var idProduct = url.searchParams.get("id");
 let article = "";
-
+const colorPicked = document. querySelector("#colors");
+const quantityPicked = document.querySelector("#quantity");
+console.log(idProduct);
 getArticle();
 
 function getArticle() {
@@ -46,8 +49,64 @@ function getPost(article){
         productColors.value = colors;
         productColors.innerHTML = colors;
     }
+    addToCart(article);
 }
 
+//Add-to-cart 
+function addToCart(article) {
+    const btn_Panier = document.querySelector("#addToCart");
+
+    btn_Panier.addEventListener("click", (event)=>{
+        if (quantityPicked.value > 0 && quantityPicked.value <=100 && quantityPicked.value != 0){
+
+    let chooseColor = colorPicked.value;
+                
+    let chooseQuantity = quantityPicked.value;
+
+    let optionsProducts = {
+        idProducts: idProduct,
+        colorProduct: chooseColor,
+        quantityProducts: Number(chooseQuantity),
+        nameProduct: article.name,
+        priceProduct: article.price,
+        descriptionProduct: article.description,
+        imgProduct: article.imageUrl,
+        altImgProduct: article.altTxt
+    };
+
+
+    let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+    const popupConfirmation =() =>{
+        if(window.confirm(`Votre commande ${chooseQuantity} ${article.name} ${chooseColor} est ajoutée au panier. Pour le consulter, cliquez sur OK`)){
+            window.location.href ="cart.html";
+        }
+    }
+
+    if (produitLocalStorage) {
+    const resultFind = produitLocalStorage.find(
+        (el) => el.idProducts=== idProduct && el.colorProduct === chooseColor);
+        if (resultFind) {
+            let newQuantity =
+            parseInt(optionsProducts.quantityProducts) + parseInt(resultFind.quantityProducts);
+            resultFind.quantityProducts = newQuantity;
+            localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+            console.table(produitLocalStorage);
+            popupConfirmation();
+        } else {
+            produitLocalStorage.push(optionsProducts);
+            localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+            console.table(produitLocalStorage);
+            popupConfirmation();
+        }
+    } else {
+        produitLocalStorage =[];
+        produitLocalStorage.push(optionsProducts);
+        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+        console.table(produitLocalStorage);
+        popupConfirmation();
+    }}
+    });
+}
 
 
 
