d95d34ae863de7448f4b59a634c8620d6b7cd582
Alice-G <so.gayat@gmail.com>
2022-06-08 12:11:40 +0200

    trying to get url - display glitch wtf

--- front/js/script.js
+++ front/js/script.js
@@ -2,86 +2,128 @@
 
 // BUG FIXME LEG BLOCK HINT TODO DEL
 
-let catalog = [];
-//HINT it has to be done INSIDE the function or it doesn't work. I don't know what to do with it.
-const fetchCatalog = function () {
-  fetch('http://localhost:3000/api/products')
-    .then(function (res) {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then(function (value) {
-      catalog = value;
-      // array catalog
-      console.log(catalog);
-      // each item in catalog
-      // for (let article of catalog) {
-      //   console.log(article);
-      // }
-    })
-    .catch(function (err) {
-      // Une erreur est survenue
-      console.log("fetch didn't work");
-    });
-};
+//BLOCK HOME PAGE
 
-const catalogDisplay = async () => {
-  await fetchCatalog();
+const apiURL = 'http://localhost:3000/api/products';
 
-  // DEL to be sure I'm actually selecting the right things
-  document.getElementById('items').style.border = '1px dashed purple';
+async function getCatalog() {
+  //storing response
+  let response = await fetch(apiURL); // stars a GET request (default)
+  // Storing data in form of JSON
+  const catalog = await response.json();
+  // console.log(catalog); //DEL
+  // console.log(typeof catalog); //DEL
+  if (response) {
+    displayCatalog(catalog);
+  }
+}
+//calling async function
+getCatalog();
 
-  document.getElementById('items').innerHTML = catalog.map((furniture) => {
-    `<a href="" id="card${furniture._id}">
-    <article>
-    <img src="${furniture._id.imageUrl}" alt="${furniture._id.altTxt}" />
-    <h3 class="productName">${furniture.name}</h3>
-    <p class="productDescription">${furniture.description}</p>
-    </article>
-    </a>
-    `;
-  });
-};
+// function to define innerHTML
+function displayCatalog(catalog) {
+  //Loop to access all rows
 
-// DEL ANOTHER attempt ANOTHER way to do it
+  for (let item of catalog) {
+    let newDiv = document.createElement('div');
+    let newContent = `<a href="./product.html?id=${item._id} data-id="${item._id}">
+      <article>
+        <img
+          src="${item.imageUrl}"
+          alt="${item.name} "
+        />
+        <h3 class="productName">${item.name}</h3>
+        <p class="productDescription">${item.description}</p>
+      </article>
+    </a>`;
 
-// We can now pass this data to a function that will render it into the HTML.
-async function displayCocktail(data) {
-  await fetchCatalog;
-  const canap = data[0];
-  const canapDiv = document.getElementById('items');
+    newDiv.innerHTML = newContent;
 
-  // Now let’s output the data into our HTML starting with the cocktail name:
-
-  const canapName = canap.name;
-  const heading = document.createElement('h1');
-  heading.innerHTML = canapName;
-  canapDiv.appendChild(heading);
-
-  // Next let’s get the image and also add it to the cocktail <div>. We’ll also use this image as the background for the <body>:
-
-  const cocktailImg = document.createElement('img');
-  cocktailImg.src = canap.imageUrl;
-  canapDiv.appendChild(cocktailImg);
-  document.body.style.backgroundImage = "url('" + canap.imageUrl + "')";
-  for (let article of catalog) {
-    displayCocktail(article);
+    //setting innerHTML as  tab
+    document.getElementById('items').appendChild(newDiv);
   }
 }
 
-/* <a href="./product.html?id=42">
-           <article>
-              <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1"/>
-              <h3  class="productName">Kanap name1</h3>
-              <p class="productDescription"></p> */
+/// A PARTIR D'ICI C'EST LA ZONE SUR LAQUELLE JE SUIS EN TRAIN DE BOSSER C'EST LE BAZAR C'EST NORMAL
 
-catalogDisplay();
+// TODO save url search param onclick?
 
-// section class="items" id="items">
-// document.getElementById(“parent”).appendChild(document.createElement(“article”));
+let chosenProduct;
+let cards = document.getElementById('items').children;
+console.log('this is cards:', cards); //DEL
 
-// i think i fetched the json. maybe it's / because of what the API param say and later I'll need to add the prodcts
-// TODO run through it and store it
-// TODO find where to display it
-// TODO  display it
+// function getProductUrl(e) {
+//   var chosenID = e.getAttribute('data-id');
+//   console.log(chosenID);
+//   chosenProduct = chosenID;
+// }
+
+// HINT I checked whether it exists first with if (cards !== null)  // not it
+for (meh of cards) {
+  console.log(meh);
+  meh.addEventListener('hover', () => {
+    console.log(meh);
+  });
+}
+
+// cards.addEventListener('click', getProductUrl);
+
+// do add event listeniner here
+
+// function storeUrl() {
+//   var str =
+//     'https://waytolearnx.com/t.html?name=alex-babtise&age=25&address=paris';
+
+//   var url = new URL(str);
+
+//   var search_params = new URLSearchParams(url.search);
+
+//   if (search_params.has('_id')) {
+//     var name = search_params.get('_id');
+//     console.log(_id);
+//     chosenProduct = _id;
+//     console.log(chosenProduct);
+//   }
+// }
+// // should it be 'items' here? or newDiv?
+
+// document.getElementById('items').addEventListener(onclick, chosenProduct);
+
+// BLOCK            PRODUCT PAGE
+
+// TODO trying to extract url from product page directly because i followed it?
+
+// window.location.href = URL;
+// console.log(URL);
+// Then you can just grab the params from the url. For instance, if the url is https://example.com/products?id=142 then you can grab the id like this:
+
+// const urlParams = new URLSearchParams(URL);
+
+// const id = urlParams.get('id');
+
+// TODO use url search param to extract id
+
+// TODO if no id, default back to home page?
+// TODO store id
+// TODO take id into the fetch GET
+
+// TODO display stuff
+
+// MAKE IT A LOOP THROUGH WITH FETCH??
+
+// DISPLAY STUFF ABOUT PRODUCT
+// product pic
+//            document.querySelector('.item_img')
+//product name
+//          document.getElementById('title').innerHTML = `${item.name}`;
+// product price
+//            id = 'price';
+// product description
+//                id = 'description';
+
+// TODO how do do colors?
+
+/* <select name="color-select" id="colors">
+<option value="">--SVP, choisissez une couleur --</option> 
+<option value="vert">vert</option>
+                      <option value="blanc">blanc</option> */
