816b8a424e21977ab14439e06a4d1e34edf75fc7
ClementFra <franquart.clemnt@gmail.com>
2022-06-14 20:32:12 +0200

    cart localstorage

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,33 +5,23 @@ var products = {}
 var totalQuantity = 0
 var totalPrice = 0
 
-document.addEventListener('DOMContentLoaded', async () => {
-  if (!location.href.includes('cart.html')) {
-    displayOrderId()
-    return
+//local storage shoppingCart
+  function getCart(){
+    let shoppingCart = localStorage.getItem("shoppingCart")
+    if (shoppingCart== null){
+      return [];
+    }else{
+      return JSON.parse(shoppingCart)
+    }
   }
-
-  products = await getProducts()
-
-  shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'))
-  if (!shoppingCart) {
-    return
+  function SaveShoppingCart(product){
+    let shoppingCart = getCart();
+    shoppingCart.push(product)
+    SaveShoppingCart(shoppingCart)
   }
-
-  const cart = document.getElementById('cart__items')
-
-  shoppingCart.forEach(async item => {
-    cart.appendChild(await createItemInCart(item))
-  })
-
-  updateTotal()
-
-  if (location.search) {
-    order()
-  }
-})
-
-const createItemInCart = async (item) => {
+   
+  // cart item 
+  const createItemInCart = (item) => {
   const product = products[item.id]
 
   const itemArticle = document.createElement('article')
@@ -86,26 +76,80 @@ const createItemInCart = async (item) => {
   itemDivContentSettingsDelText.addEventListener('click', deleteItem)
   itemDivContentSettingsDelText.innerHTML = 'Supprimer'
 
-  itemDivContentDesc.appendChild(itemDivContentDescName)
-  itemDivContentDesc.appendChild(itemDivContentDescColor)
-  itemDivContentDesc.appendChild(itemDivContentDescPrice)
-
-  itemDivContentSettingsQty.appendChild(itemDivContentSettingsQtyValue)
-  itemDivContentSettingsQty.appendChild(itemDivContentSettingsQtyInput)
-
-  itemDivContentSettingsDel.appendChild(itemDivContentSettingsDelText)
-
-  itemDivContentSettings.appendChild(itemDivContentSettingsQty)
-  itemDivContentSettings.appendChild(itemDivContentSettingsDel)
-
-  itemDivContent.appendChild(itemDivContentDesc)
-  itemDivContent.appendChild(itemDivContentSettings)
-
-  itemDivImg.appendChild(itemImg)
-
-  itemArticle.appendChild(itemDivImg)
-  itemArticle.appendChild(itemDivContent)
-
   return itemArticle
 }
+//delete item
+const deleteItem = (delButton) => {
+  if (window.confirm('Êtes-vous sûr de vouloir supprimer ce produit du panier ?')) {
+    const path = delButton.path || (delButton.composedPath && delButton.composedPath())
+    const cartItem = path.find(element => element.classList.contains('cart__item'))
+    const id = cartItem.dataset.id
+    const color = cartItem.dataset.color
+    cartItem.parentNode.removeChild(cartItem)
+    shoppingCart.splice(shoppingCart.indexOf(shoppingCart.find(item => item.id === id && item.color === color)), 1)
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart))
+    alert('Suppression.')
+    updateTotal()
+  }
+}
+// informations customer
+  const searchParams = new URLSearchParams(window.location.search)
+  const firstName = searchParams.get('firstName')
+  const lastName = searchParams.get('lastName')
+  const address = searchParams.get('address')
+  const city = searchParams.get('city')
+  const email = searchParams.get('email')
+
+  const firstNameErrField = document.getElementById('firstNameErrorMsg')
+  const lastNameErrField = document.getElementById('lastNameErrorMsg')
+  const addressErrField = document.getElementById('addressErrorMsg')
+  const cityErrField = document.getElementById('cityErrorMsg')
+  const emailErrField = document.getElementById('emailErrorMsg')
+
+  const nameRegex = /^[A-zÀ-ú' -]*$/
+  const addressRegex = /([0-9]{1,}) ?([A-zÀ-ú,' -\. ]*)/
+  const mailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  let error = false
+
+  if (firstName) {
+    if (!firstName.match(nameRegex)) {
+      firstNameErrField.innerHTML = 'Saisir un prénom valide.'
+      error = true
+    }
+  }
+
+  if (lastName) {
+    if (!lastName.match(nameRegex)) {
+      lastNameErrField.innerHTML = 'Saisir un nom valide.'
+      error = true
+    }
+  }
+
+  if (address) {
+    if (!address.match(addressRegex)) {
+      addressErrField.innerHTML = 'Saisir une adresse valide.'
+      error = true
+    }
+  }
+
+  if (city) {
+    if (!city.match(nameRegex)) {
+      cityErrField.innerHTML = 'Saisir un nom de ville valide.'
+      error = true
+    }
+  }
+
+  if (email) {
+    if (!email.match(mailRegex)) {
+      emailErrField.innerHTML = 'Saisir une adresse email valide.'
+      error = true
+    }
+  }
+
+
+
+
+
+
 
