fbd7bfb64da9d6f7d1c6c6dcf4fd52b2f5cc1b97
BricePou <brice.poullain@sfr.fr>
2023-01-30 09:22:02 +0100

    go to correction

--- front/js/product.js
+++ front/js/product.js
@@ -3,7 +3,7 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
 const productId = urlParams.get ('id')
 console.log(productId)
-// On demande a l'API de nous retourner l'ID du produit,(qu'est ce que le localhost)
+// On demande a l'API de nous retourner l'ID du produit,(qu'est ce que le localhost) on retounre les odnné du produit
 fetch (`http://localhost:3000/api/products/${productId}`)
 //.then fais un appel a l'api, il demande une reponse 
 .then((resp)=>{
@@ -15,7 +15,6 @@ fetch (`http://localhost:3000/api/products/${productId}`)
 })
 //
 .then((product)=>{
-    console.log(product)
     // On créé le code HTML en demandant a l'API de nous retourner l'image et la description du produit
     document.querySelector('.item__img').innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
     // de nous inséré le titre du produit
@@ -23,16 +22,16 @@ fetch (`http://localhost:3000/api/products/${productId}`)
     // de nous inséré la description du produit
     document.getElementById('description').innerText = product.description
     // et enfin chaque couleur du produit       prdocut.color.forEach value color aussi
-    document.getElementById('colors').innerHTML +=
-    product.color.forEach(element => {`<option value="vert">${product.colors}</option>`
-        
+    
+    product.colors.forEach(color => {
+        document.getElementById('colors').innerHTML += `<option value="${color}">${color}</option>`   
     });
-    `<option value="vert">${product.colors}</option>`;
 
 
     //ajout du clique bouton dans le then
-    document.getElementById('addToCart').addEventListener('click',function (e){
-        addCart(`${productId}`)
+    document.getElementById('addToCart')
+    .addEventListener('click',function (e){
+        addCart(${productId})
       })
 })
 //
@@ -40,6 +39,11 @@ fetch (`http://localhost:3000/api/products/${productId}`)
 // classe item (faire la catch error sur lma class item)
 });
 //(explication ligne par ligne)
+
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
 function getCart() {
     let cart = localStorage.getItem('cart');
     if (cart == null){
@@ -50,28 +54,38 @@ function getCart() {
 }
 //(explication ligne par ligne  et ajout des)
 function addCart(product) {
+    // One recuepàre le contenu du panier local storage
     let cart = getCart();
+
     const color = document.getElementById("colors").value;
     const quantity = document.getElementById("quantity").value;
-
+    //On récupere que la quantité est valide
     if(quantity < 1 || quantity > 100){
-        alert ('La quantité doit être située entre 1 et 100')
+        alert ('La quantité doit être située entre 1 et 100');
+        return;
     }
-
-    let foundProduct = cart.find(p => p.id == product.id);
+    //Verifier qu'un cas a etait choisie if pas de couleur alors je return
+//
+    let foundProduct = cart.find(p => p._id == product._id && p.color === color);
     if(foundProduct != undefined){
+        //parse int car on repasse en nombre et oas 
       foundProduct.quantity+= parseint(quantity);
     }else{
         //produit formater. Recuperation des variables utile(enlever la possibliter de changer le prix dans le local host)
        const formattedProduct = {
-        "_id":productId,
+        "_id":product._id,
         "color": color,
         "quantity": parseInt(quantity)
        }
 
        // "id" product id
        //const color et quantity  document.getElementById("colors").value
-      cart.push(product);//produit formater
+       //
+      cart.push(formattedProduct);
     }
+    // On met a jour le local storage
     saveCart(cart);
 }
+function render product
+
+//sort js
